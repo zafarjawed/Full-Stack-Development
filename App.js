d0474ffa4a81918d4ec9883adaeec6ba91b4423f
Md Zafar Jawed <zafarjawed038@gmail.com>
2026-02-09 11:23:30 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,115 @@
+// Case Study 10- EMP details using React
+
+
+// import React from 'react'; 
+// import"./style.css" 
+// export default function Head (){ 
+ 
+//   return<div> 
+//      <h1>Employee Details</h1> 
+//      <p>---------------</p> 
+//      <h4>Employee Id: 111</h4> 
+//     <h4>Employee Name: Md Zafar Jawed</h4> 
+//     <h4>Employee Company: xyz</h4> 
+//     <h4>Employee Salary: 90000</h4> 
+//     <h4>Employee Location: Bengaluru</h4>  
+//    <h4>Employee Phone: 1234567890</h4>  
+//     </div> 
+// }; 
+// // export default Head; 
+
+
+// Case Study 11- Props and State using React 
+ 
+ 
+// import React from 'react'; 
+ 
+// function Parent(props){ 
+//   return<h1>Hello,{props.name}</h1> 
+// }; 
+// export default Child 
+// function Child(){ 
+//   return ( 
+//     <Parent   name="Zafar" />  // concept of jsx (javascirpt xml) 
+//   ) 
+// }  
+ 
+ // // State 
+ 
+//   import React , {useState} from 'react'; 
+ //   function State(){ 
+ //     const[message,setMessage] = useState("Hello") 
+     //     return <div> 
+ //     <h1>{message}</h1> 
+ //     <button 
+//     onClick = {()=>setMessage("Zafar")} 
+//     >Change Text</button> 
+//     </div> 
+//   }; 
+ //   export default State
+
+
+//  Mini Project 11- Youtube Login Page using React 
+
+// import './style.css'; 
+// function YouTube() { 
+// return ( 
+// <div className="container"> 
+// <div className="login-card"> 
+// <h1>YouTube</h1> 
+// <input type="text" placeholder="Your username" /> 
+// <input type="password" placeholder="Password" /> 
+// <div className="options"> 
+// <label> 
+// <input type="checkbox" /> Remember me 
+// </label> 
+// <span>Forgot Password?</span> 
+// </div> 
+// <button>Log In</button> 
+// <p className="signup"> 
+// Don’t have an account? <span>Sign Up</span> 
+// </p> 
+// </div> 
+// </div> 
+// ); 
+// } 
+// export default YouTube; 
+
+
+// Mini Project 12-  Count Buffering System 
+ 
+ 
+// import React, { useState } from "react"; 
+// import "./style.css"; 
+ 
+// function Counter() { 
+//   const [count, setCount] = useState(0); 
+ 
+//   return ( 
+//     <div> 
+//       <h1 className="cou">Buffer System</h1> 
+ 
+//       <p className="cou">{count}</p> 
+ 
+//       <button className="center" onClick={() => setCount(count - 1)}> 
+//         Decrement 
+//       </button> 
+ 
+//       <button className="center" onClick={() => setCount(count + 1)}> 
+//         Increment 
+//       </button> 
+ 
+//       <button className="center" onClick={() => setCount(0)}> 
+//         Reset 
+//       </button> 
+//     </div> 
+//   ); 
+// } 
+ 
+// export default Counter; 
+ 
+
+
+
+
+ 
